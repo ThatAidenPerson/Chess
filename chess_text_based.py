3974e540b759d6dcbf1f4820f1a8c64dc6d13764
That Aiden Person <aidencoates@exe-coll.ac.uk>
2024-02-28 00:47:24 +0000

    currently working on adding pgn to ctb

--- chess_text_based.py
+++ chess_text_based.py
@@ -1,9 +1,11 @@
-import chess
+import chess, chess.pgn
 from stockfish import Stockfish
 import os, time
-
+from datetime import datetime
 # Initializes an empty chess board
 board = chess.Board()
+game = chess.pgn.Game()
+node = game
 stockfish = Stockfish(r"C:\stockfish\stockfish-windows-x86-64-avx2.exe") #for college, change to M at the start, for home, change to C
 fen = board.fen() 
 
@@ -11,6 +13,16 @@ fen = board.fen()
 forfiet = ["forfiet", "quit", "exit", "surrender", "give up", "resign"]
 vs_bot = ["bot", "computer", "ai", "stockfish", "1"]
 vs_player = ["player", "human", "2"]
+who_turn = []
+
+def set_headers(winner, white, black):
+    node.headers["Event"] = "Chess Game"
+    node.headers["Site"] = "Aiden's PyChess Game"
+    node.headers["Date"] = datetime.now().strftime("%Y.%m.%d")
+    node.headers["Round"] = "To be determined"
+    node.headers["White"] = white
+    node.headers["Black"] = black
+    node.headers["Result"] = winner
 
 def print_board():
     print(board)
@@ -23,6 +35,7 @@ def print_board():
 def game():   
     
     player_name = input("What is your name? ").title()
+    
     os.system("cls")
     print("Welcome to Chess, " + player_name + ".")
     game_mode = input("""What would you like to do? 
@@ -34,7 +47,7 @@ def game():
     if game_mode in vs_player:
         player_name2 = input("What is the other player's name? ").title()
         while True:
-            b_or_w = input(f"Who would you like to play as white or black? (1: {player_name}, 2: {player_name2})").title()
+            b_or_w = input(f"Who would you like to play as white? (1: {player_name}, 2: {player_name2})").title()
             if b_or_w in ["1", player_name]:
                 who_turn = [player_name, player_name2]
                 break
@@ -45,8 +58,10 @@ def game():
                 print("Invalid input. Try again.")
                 continue
             
+            
     
     while True:
+        
         turn = board.turn
 
         print_board()
@@ -65,11 +80,16 @@ def game():
             continue
         elif move in forfiet:   
             print("You have forfieted the game. You lose!")
+            if turn == True:
+                set_headers("0-1", who_turn[1], who_turn[0])
+            else:
+                set_headers("1-0", who_turn[0], who_turn[1])
             break
         elif chess.Move.from_uci(move) in board.legal_moves:
             board.push_uci(move)
             fen = board.fen()
             turn = board.turn
+            game = node.add_variation(chess.Move.from_uci(move))
             os.system("cls")
         else:
             print("Invalid move. Try again.")
@@ -86,14 +106,20 @@ def game():
         if board.is_checkmate() == True:
             if turn == True:
                 print("Checkmate, Black wins!")
+                set_headers("0-1", who_turn[1], who_turn[0])
             else:
                 print("Checkmate, White wins!")
+                set_headers("1-0", who_turn[0], who_turn[1])
             break
+    
+    # Save the game to a PGN file
+    with open("game.pgn", "w") as pgn_file:
+        print(game, file=pgn_file)
 
 
 game()
 
 
-
+# Set up something for when the game draws
 
 
